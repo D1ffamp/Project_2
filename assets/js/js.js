c370ac8467581279aec5488063f44f21e297f934
D1ffamp <marcam82@gmail.com>
2022-07-20 14:03:41 +0000

    created function in javascript to generate and display opponent choice

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,30 @@
+//global variables and constants for DOM elements
+let comptuterChoice
+const opponentChoice = document.getElementById("opponent-choice")
+
 document.addEventListener("DOMContentLoaded",function(){
     let buttons = this.getElementsByTagName("button");
         for(let button of buttons){
             button.addEventListener("click",function(){
-                alert("you clicked")
+                let choice = this.getAttribute("id");
+                alert(`you clicked ${choice}`);
+                opponent()
             })
         }
 })
+
+function opponent(){
+    const randomNumber = Math.floor(Math.random()*3) +1
+
+    if(randomNumber === 1){
+        computerChoice ="rock"
+    }
+    if(randomNumber === 2){
+        computerChoice ="paper"
+    }
+    if(randomNumber === 3){
+        computerChoice ="scissors"
+    }
+
+    opponentChoice.innerHTML = computerChoice
+}
